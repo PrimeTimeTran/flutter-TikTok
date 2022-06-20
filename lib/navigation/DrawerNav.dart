71bca1e6636f325cd5b8d814c5974711314c7d73
Loi Van Tran <datloiboi@gmail.com>
2022-06-20 13:50:54 +0700

    Refactor tab into own nav

--- lib/navigation/DrawerNav.dart
+++ lib/navigation/DrawerNav.dart
@@ -1,64 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:worldlingo3/pages/Newsfeed.dart';
+import 'package:worldlingo3/navigation/TabNav.dart';
 
-class StatefulTabBar extends StatefulWidget {
-  const StatefulTabBar({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulTabBar> createState() => _StatefulTabBar();
-}
-
-class _StatefulTabBar extends State<StatefulTabBar> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Newsfeed(),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
 
 class DrawerNav extends StatelessWidget {
   const DrawerNav({Key? key, required this.title}) : super(key: key);
