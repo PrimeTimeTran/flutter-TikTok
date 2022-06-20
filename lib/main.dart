2595f4ecf8613981ce2d58380e1220fd2b0b120a
Loi Van Tran <datloiboi@gmail.com>
2022-06-20 11:28:06 +0700

    Refactor for addiitiiionnal layouts

--- lib/main.dart
+++ lib/main.dart
@@ -29,59 +29,109 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
       debugShowCheckedModeBanner: false,
       color: Colors.red,
-      title: 'Layouts',
-      home: Container(
-        color: Colors.yellow,
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(5),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  margin: EdgeInsets.all(20.0),
-                  padding: EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)),
-                  child: Column(
-                    children: [
-                      Box(colour: Colors.red),
-                      Box(colour: Colors.white),
-                      Box(colour: Colors.blue),
-                    ],
-                  ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('World Wide Words'),
+          ),
+          body: Container(
+            color: Colors.yellow,
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(5),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 20.0),
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blueAccent)),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Box(colour: Colors.pink),
+                          Box(colour: Colors.cyanAccent),
+                          Box(colour: Colors.deepPurple),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20.0),
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blueAccent)),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Box(colour: Colors.pink),
+                          Box(colour: Colors.cyanAccent),
+                          Box(colour: Colors.deepPurple),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20.0),
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blueAccent)),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Box(colour: Colors.pink),
+                          Box(colour: Colors.cyanAccent),
+                          Box(colour: Colors.deepPurple),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(20.0),
+                      padding: const EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blueAccent)),
+                      child: Column(
+                        children: [
+                          Box(colour: Colors.red),
+                          Box(colour: Colors.white),
+                          Box(colour: Colors.blue),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(20.0),
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blueAccent)),
+                      child: Column(
+                        children: [
+                          Box(colour: Colors.amber),
+                          Box(colour: Colors.black),
+                          Box(colour: Colors.green),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20.0),
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blueAccent)),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Box(colour: Colors.pink),
+                          Box(colour: Colors.cyanAccent),
+                          Box(colour: Colors.deepPurple),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Container(
-                  margin: EdgeInsets.all(20.0),
-                  padding: EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)),
-                  child: Column(
-                    children: [
-                      Box(colour: Colors.amber),
-                      Box(colour: Colors.black),
-                      Box(colour: Colors.green),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20.0),
-                  padding: EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Box(colour: Colors.pink),
-                      Box(colour: Colors.cyanAccent),
-                      Box(colour: Colors.deepPurple),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
-      ));
+      );
 }
