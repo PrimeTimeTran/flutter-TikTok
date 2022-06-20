3e275cad985b535cbe1652891d08a8d1e33ce42e
Loi Van Tran <datloiboi@gmail.com>
2022-06-20 13:43:33 +0700

    Nest pages in tabs successfully

--- lib/navigation/DrawerNav.dart
+++ lib/navigation/DrawerNav.dart
@@ -1,145 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:worldlingo3/widgets/Box.dart';
 
-class Newsfeed extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Container(
-        color: Colors.yellow,
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(5),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 20.0),
-                  padding: EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Box(colour: Colors.red.shade900),
-                      Box(colour: Colors.white),
-                      Box(colour: Colors.blue.shade900),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20.0),
-                  padding: EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Box(colour: Colors.pink),
-                      Box(colour: Colors.cyanAccent),
-                      Box(colour: Colors.deepPurple),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20.0),
-                  padding: EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Box(colour: Colors.pink),
-                      Box(colour: Colors.cyanAccent),
-                      Box(colour: Colors.deepPurple),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(20.0),
-                  padding: const EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)),
-                  child: Column(
-                    children: [
-                      Box(colour: Colors.red),
-                      Box(colour: Colors.white),
-                      Box(colour: Colors.blue),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(20.0),
-                  padding: EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)),
-                  child: Column(
-                    children: [
-                      Box(colour: Colors.amber),
-                      Box(colour: Colors.black),
-                      Box(colour: Colors.green),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20.0),
-                  padding: EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Box(colour: Colors.pink),
-                      Box(colour: Colors.cyanAccent),
-                      Box(colour: Colors.deepPurple),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TabBarDemo extends StatelessWidget {
-  const TabBarDemo({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ],
-            ),
-            title: const Text('Tabs Demo'),
-          ),
-          body: const TabBarView(
-            children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+import 'package:worldlingo3/pages/Newsfeed.dart';
 
 class StatefulTabBar extends StatefulWidget {
   const StatefulTabBar({Key? key}) : super(key: key);
@@ -153,10 +14,7 @@ class _StatefulTabBar extends State<StatefulTabBar> {
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Hom1111e',
-      style: optionStyle,
-    ),
+    Newsfeed(),
     Text(
       'Index 1: Business',
       style: optionStyle,
@@ -212,6 +70,7 @@ class DrawerNav extends StatelessWidget {
       appBar: AppBar(
         title: const Text('World Wide Words'),
       ),
+      // body: Newsfeed(),
       body: StatefulTabBar(),
       drawer: Drawer(
         child: ListView(
