7a55e4baf988c561055b299c1a9de7e1d1a7e49b
Loi Van Tran <datloiboi@gmail.com>
2022-06-22 11:27:33 +0700

    Implement CustomListTile

--- lib/model/phrase.dart
+++ lib/model/phrase.dart
@@ -4,41 +4,48 @@ class PhraseFields {
   static const String id = 'id';
   static const String vi = 'vi';
   static const String en = 'en';
+  static const String author = 'author';
 
-  static List<String> getFields() => [id, vi, en];
+  static List<String> getFields() => [id, vi, en, author];
 }
 
 class Phrase {
-  final String? id;
+  final String id;
   final String en;
   final String vi;
+  final String author;
 
   const Phrase({
-    this.id,
+    required this.id,
     required this.en,
     required this.vi,
+    required this.author,
   });
 
   Phrase copy({
     String? id,
     String? vi,
     String? en,
+    String? author,
   }) =>
       Phrase(
         id: id ?? this.id,
         en: en ?? this.en,
         vi: vi ?? this.vi,
+        author: author ?? this.author,
       );
 
   static Phrase fromJson(Map<String, dynamic> json) => Phrase(
         id: json[PhraseFields.id],
         en: json[PhraseFields.en],
         vi: json[PhraseFields.vi],
+        author: json[PhraseFields.author],
       );
 
   Map<String, dynamic> toJson() => {
         PhraseFields.id: id,
         PhraseFields.en: en,
         PhraseFields.vi: vi,
+        PhraseFields.author: author,
       };
 }
--- lib/pages/Phrasefeed.dart
+++ lib/pages/Phrasefeed.dart
@@ -37,16 +37,145 @@ class _Phrasefeed extends State<Phrasefeed> {
         color: Colors.white,
         padding: const EdgeInsets.all(5),
         child: ListView.builder(
+          padding: const EdgeInsets.all(10.0),
           itemCount: phrases.length,
           itemBuilder: (context, index) {
-            final icon = Icon(Icons.lightbulb, color: Colors.red, size: 30);
-            return ListTile(
-              title: Text(phrases[index].vi),
-              subtitle: Text(phrases[index].en),
-              isThreeLine: true,
-              leading: icon,
+
+            return CustomListTile(
+              thumbnail: Container(
+                decoration: const BoxDecoration(color: Colors.pink),
+              ),
+              title: phrases[index].en,
+              subtitle: phrases[index].vi,
+              // 'This text should max out at two lines and clip',
+              author: phrases[index].author,
+              publishDate: 'Dec 28',
+              readDuration: '5 mins',
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class _ArticleDescription extends StatelessWidget {
+  const _ArticleDescription({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.author,
+    required this.publishDate,
+    required this.readDuration,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 2.0)),
+              Text(
+                subtitle,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                author,
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(
+                '$publishDate - $readDuration',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  const CustomListTile({
+    Key? key,
+    required this.thumbnail,
+    required this.title,
+    required this.subtitle,
+    required this.author,
+    required this.publishDate,
+    required this.readDuration,
+  }) : super(key: key);
+
+  final Widget thumbnail;
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1.0,
+              child: thumbnail,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+                child: _ArticleDescription(
+                  title: title,
+                  subtitle: subtitle,
+                  author: author,
+                  publishDate: publishDate,
+                  readDuration: readDuration,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
