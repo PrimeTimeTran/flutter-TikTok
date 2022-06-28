36c17904121184b2ebbf6b215ee061a2ffd73778
Loi Van Tran <datloiboi@gmail.com>
2022-06-28 12:08:39 +0700

    Complete right to left animation

--- lib/pages/DiscoverPage.dart
+++ lib/pages/DiscoverPage.dart
@@ -7,13 +7,16 @@ class CustomPageRoute extends PageRouteBuilder {
   CustomPageRoute({
     required this.child,
   }) : super(
-            transitionDuration: const Duration(seconds: 10),
+            transitionDuration: const Duration(seconds: 1),
             pageBuilder: (context, animation, secondaryAnimation) => child);
 
   @override
   Widget buildTransitions(BuildContext context, Animation<double> animation,
           Animation<double> secondaryAnimation, Widget child) =>
-      ScaleTransition(scale: animation, child: child);
+      SlideTransition(
+          position: Tween<Offset>(begin: const Offset(1, 0), end: Offset.zero)
+              .animate(animation),
+          child: child);
 }
 
 class DiscoverPage extends StatefulWidget {
