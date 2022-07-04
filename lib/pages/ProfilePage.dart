1fbad47b3240d55679edac38d3b68ec86b23f267
Loi Van Tran <datloiboi@gmail.com>
2022-07-04 12:08:25 +0700

    WIP: Add nested tab in profile screen

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -7,6 +7,169 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePage();
 }
 
+class MyHomePage extends StatefulWidget {
+  // const MyHomePage({Key key, this.title}) : super(key: key);
+
+  // final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hello World"),
+      ),
+      bottomNavigationBar: Material(
+        color: Colors.white,
+        child: TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.teal,
+            labelColor: Colors.teal,
+            unselectedLabelColor: Colors.black54,
+            tabs: const <Widget>[
+              Tab(
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                icon: Icon(Icons.email),
+              ),
+              Tab(
+                icon: Icon(Icons.settings),
+              ),
+            ]),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const <Widget>[
+          Center(
+            child: Text("Home"),
+          ),
+          Center(
+            child: Text("Email"),
+          ),
+          Center(
+            child: Text("Settings"),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class NestedTabBar extends StatefulWidget {
+  @override
+  _NestedTabBarState createState() => _NestedTabBarState();
+}
+
+class _NestedTabBarState extends State<NestedTabBar>
+    with TickerProviderStateMixin {
+  late TabController _nestedTabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _nestedTabController = TabController(length: 5, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nestedTabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        TabBar(
+          controller: _nestedTabController,
+          indicatorColor: Colors.teal,
+          labelColor: Colors.teal,
+          unselectedLabelColor: Colors.black54,
+          isScrollable: true,
+          tabs: const <Widget>[
+            Tab(
+              text: "One",
+            ),
+            Tab(
+              text: "Two",
+            ),
+            Tab(
+              text: "Three",
+            ),
+            Tab(
+              text: "Four",
+            ),
+            Tab(
+              text: "Five",
+            ),
+          ],
+        ),
+        Container(
+          height: screenHeight * 0.70,
+          margin: const EdgeInsets.only(left: 16.0, right: 16.0),
+          child: TabBarView(
+            controller: _nestedTabController,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: Colors.blueAccent,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: Colors.orangeAccent,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: Colors.greenAccent,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: Colors.indigoAccent,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: Colors.redAccent,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
 
 class _ProfilePage extends State<ProfilePage> {
   final int _count = 0;
@@ -14,81 +177,79 @@ class _ProfilePage extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ListView(
-          children: [
-            Column(
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(vertical: 20),
-                      width: 100.0,
-                      height: 100.0,
-                      decoration: const BoxDecoration(
-                        color: Colors.blue,
-                        shape: BoxShape.circle,
-                      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 20),
+                    width: 100.0,
+                    height: 100.0,
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
                     ),
-                    const Text('PrimeTimeTran'),
-                    Padding(
-                      padding: const EdgeInsets.all(30),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Column(
-                            children: const [
-                              Text("117"),
-                              Text("Following"),
-                            ],
-                          ),
-                          Column(
-                            children: const [
-                              Text("117"),
-                              Text("Following"),
-                            ],
-                          ),
-                          Column(
-                            children: const [
-                              Text("117"),
-                              Text("Following"),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    OutlinedButton(
-                        style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue),
-                          overlayColor:
-                              MaterialStateProperty.resolveWith<Color?>(
-                            (Set<MaterialState> states) {
-                              if (states.contains(MaterialState.hovered)) {
-                                return Colors.blue.withOpacity(0.04);
-                              }
-                              if (states.contains(MaterialState.focused) ||
-                                  states.contains(MaterialState.pressed)) {
-                                return Colors.blue.withOpacity(0.12);
-                              }
-                              return null; // Defer to the widget's default.
-                            },
-                          ),
+                  ),
+                  const Text('PrimeTimeTran'),
+                  Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          children: const [
+                            Text("117"),
+                            Text("Following"),
+                          ],
                         ),
-                        onPressed: () {},
-                        child: const Text('TextButton'))
-                  ],
-                )
-              ],
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.green,
-                child: const Text('Hello World'),
-              ),
-            ),
-          ],
-        ),
+                        Column(
+                          children: const [
+                            Text("117"),
+                            Text("Following"),
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            Text("117"),
+                            Text("Following"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  OutlinedButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                      overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered)) {
+                            return Colors.blue.withOpacity(0.04);
+                          }
+                          if (states.contains(MaterialState.focused) ||
+                              states.contains(MaterialState.pressed)) {
+                            return Colors.blue.withOpacity(0.12);
+                          }
+                          return null; // Defer to the widget's default.
+                        },
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text('TextButton'),
+                  ),
+                ],
+              )
+            ],
+          ),
+          Container(
+            color: Colors.blue,
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: const Text('Hello World'),
+          ),
+          NestedTabBar(),
+        ],
       ),
     );
   }
