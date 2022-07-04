a597d261bd05ad3c4a3f8e409416336ce489ad34
Loi Van Tran <datloiboi@gmail.com>
2022-07-04 11:52:13 +0700

    WIP: Profile page

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -7,13 +7,89 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePage();
 }
 
+
 class _ProfilePage extends State<ProfilePage> {
   final int _count = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('You have pressed the button $_count times.')),
+      body: Center(
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 20),
+                      width: 100.0,
+                      height: 100.0,
+                      decoration: const BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    const Text('PrimeTimeTran'),
+                    Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            children: const [
+                              Text("117"),
+                              Text("Following"),
+                            ],
+                          ),
+                          Column(
+                            children: const [
+                              Text("117"),
+                              Text("Following"),
+                            ],
+                          ),
+                          Column(
+                            children: const [
+                              Text("117"),
+                              Text("Following"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    OutlinedButton(
+                        style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.blue),
+                          overlayColor:
+                              MaterialStateProperty.resolveWith<Color?>(
+                            (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.hovered)) {
+                                return Colors.blue.withOpacity(0.04);
+                              }
+                              if (states.contains(MaterialState.focused) ||
+                                  states.contains(MaterialState.pressed)) {
+                                return Colors.blue.withOpacity(0.12);
+                              }
+                              return null; // Defer to the widget's default.
+                            },
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text('TextButton'))
+                  ],
+                )
+              ],
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.green,
+                child: const Text('Hello World'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
