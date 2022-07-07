bfa8d6ee728c804aec7f8cdfd254884a5de6dbc0
Loi Van Tran <datloiboi@gmail.com>
2022-07-07 13:01:54 +0700

    Test: Autoplay on PWA

--- lib/widgets/MediaContent.dart
+++ lib/widgets/MediaContent.dart
@@ -90,147 +90,146 @@ class _MediaContentState extends State<MediaContent> {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
     final i = widget.index;
-    return Container(
-      width: width,
-      height: height,
-      color: _contentTypeList[i].materialColor,
-      child: Stack(
-        children: [
-          // VideoApp(url: _contentTypeList[i].mediaUrl),
-          _controller.value.isInitialized
-              ? SizedBox(
-                  height: height,
-                  width: width,
-                  child: VideoPlayer(_controller),
-                )
-              : Container(),
-          Container(
-            alignment: Alignment.bottomCenter,
-            color: Colors.transparent,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: Container(
-                    alignment: Alignment.bottomLeft,
-                    padding: const EdgeInsets.only(bottom: 30, left: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          _contentTypeList[i].creatorName,
-                          style: const TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              decoration: TextDecoration.none),
-                        ),
-                        Text(
-                          _contentTypeList[i].caption,
-                          style: const TextStyle(
-                              fontSize: 15,
-                              color: Colors.grey,
-                              decoration: TextDecoration.none),
-                        ),
-                        Row(
-                          children: [
-                            const Icon(Icons.my_library_music),
-                            Text(
-                              _contentTypeList[i].soundTitle,
-                              style: const TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.grey,
-                                  decoration: TextDecoration.none),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    alignment: Alignment.centerRight,
-                    height: height / 1.8,
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 5),
+    return GestureDetector(
+      child: Container(
+        width: width,
+        height: height,
+        color: _contentTypeList[i].materialColor,
+        child: Stack(
+          children: [
+            // VideoApp(url: _contentTypeList[i].mediaUrl),
+            _controller.value.isInitialized
+                ? SizedBox(
+                    height: height,
+                    width: width,
+                    child: VideoPlayer(_controller),
+                  )
+                : Container(),
+            Container(
+              alignment: Alignment.bottomCenter,
+              color: Colors.transparent,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      alignment: Alignment.bottomLeft,
+                      padding: const EdgeInsets.only(bottom: 30, left: 10),
                       child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          GestureDetector(
-                            child: const Icon(Icons.person_pin_circle_sharp),
-                            onTap: () {
-                              print('Tapped to play');
-                              _controller.play();
-                            },
+                          Text(
+                            _contentTypeList[i].creatorName,
+                            style: const TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                decoration: TextDecoration.none),
                           ),
-                          const Icon(Icons.person_pin_circle_sharp),
-                          Column(
+                          Text(
+                            _contentTypeList[i].caption,
+                            style: const TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey,
+                                decoration: TextDecoration.none),
+                          ),
+                          Row(
                             children: [
-                              const Icon(Icons.heart_broken),
+                              const Icon(Icons.my_library_music),
                               Text(
-                                _contentTypeList[i].countLikes.toString(),
+                                _contentTypeList[i].soundTitle,
                                 style: const TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                  decoration: TextDecoration.none,
-                                ),
-                              ),
+                                    fontSize: 15,
+                                    color: Colors.grey,
+                                    decoration: TextDecoration.none),
+                              )
                             ],
-                          ),
-                          Column(
-                            children: [
-                              const Icon(Icons.message),
-                              Text(
-                                _contentTypeList[i].countComments.toString(),
-                                style: const TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                  decoration: TextDecoration.none,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              const Icon(Icons.bookmark_outlined),
-                              Text(
-                                _contentTypeList[i].countBookmarks.toString(),
-                                style: const TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                  decoration: TextDecoration.none,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              const Icon(Icons.share_sharp),
-                              Text(
-                                _contentTypeList[i].countShares.toString(),
-                                style: const TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                  decoration: TextDecoration.none,
-                                ),
-                              ),
-                            ],
-                          ),
-                          const Icon(Icons.music_video),
+                          )
                         ],
                       ),
                     ),
                   ),
-                ),
-              ],
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      alignment: Alignment.centerRight,
+                      height: height / 1.8,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 5),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Icon(Icons.person_pin_circle_sharp),
+                            Column(
+                              children: [
+                                const Icon(Icons.heart_broken),
+                                Text(
+                                  _contentTypeList[i].countLikes.toString(),
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                    decoration: TextDecoration.none,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                const Icon(Icons.message),
+                                Text(
+                                  _contentTypeList[i].countComments.toString(),
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                    decoration: TextDecoration.none,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                const Icon(Icons.bookmark_outlined),
+                                Text(
+                                  _contentTypeList[i].countBookmarks.toString(),
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                    decoration: TextDecoration.none,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                const Icon(Icons.share_sharp),
+                                Text(
+                                  _contentTypeList[i].countShares.toString(),
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                    decoration: TextDecoration.none,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Icon(Icons.music_video),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+      onTap: () {
+        print('outside');
+        _controller.play();
+      },
     );
   }
 }
