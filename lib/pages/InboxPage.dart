3b18a961932543509b3821ab06899753ca12ca70
Loi Van Tran <datloiboi@gmail.com>
2022-07-10 12:10:21 +0700

    Add: Vertical scroll on InboxPage

--- lib/pages/InboxPage.dart
+++ lib/pages/InboxPage.dart
@@ -10,70 +10,67 @@ class InboxPage extends StatefulWidget {
 class _InboxPage extends State<InboxPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 100,
-              child: ListView.builder(
-                itemCount: 10,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (ctx, i) {
-                  return Padding(
-                    padding: const EdgeInsets.all(5),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (ctx, i) {
+                return Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundImage:
+                        NetworkImage('https://picsum.photos/250?image=$i'),
+                  ),
+                );
+              },
+            ),
+          ),
+          Row(children: const [Text('Messages')]),
+          ListView.separated(
+            shrinkWrap: true,
+            itemCount: 20,
+            separatorBuilder: (context, index) {
+              return const Divider();
+            },
+            itemBuilder: (ctx, i) {
+              return Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
                     child: CircleAvatar(
                       radius: 30,
                       backgroundImage:
                           NetworkImage('https://picsum.photos/250?image=$i'),
                     ),
-                  );
-                },
-              ),
-            ),
-            Row(children: const [Text('Messages')]),
-            Expanded(
-              child: ListView.separated(
-                itemCount: 20,
-                separatorBuilder: (context, index) {
-                  return const Divider();
-                },
-                itemBuilder: (ctx, i) {
-                  return Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: NetworkImage(
-                              'https://picsum.photos/250?image=$i'),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Column(
+                          children: const [
+                            Text('Hslslslsl'),
+                            Text('Hslslslsl')
+                          ],
                         ),
                       ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Column(
-                              children: const [
-                                Text('Hslslslsl'),
-                                Text('Hslslslsl')
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Text('Hslslslsl'),
-                      )
-                    ],
-                  );
-                },
-              ),
-            )
-          ],
-        ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('Hslslslsl'),
+                  )
+                ],
+              );
+            },
+          ),
+        ],
       ),
     );
   }
